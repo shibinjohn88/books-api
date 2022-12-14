0cebe3c3bc07d999c29b45df61fda173de510516
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-12-13 22:03:16 -0500

    cors installed

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const mongoose = require ('mongoose')
+const cors = require ('cors')
 
 //configuration
 const app = express ()
 require ('dotenv').config()
 app.use (express.json())
+
+
+//Enable all CORS requests
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 
 //Index
