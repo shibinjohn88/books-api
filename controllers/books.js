09694776b5cbf8e84d05f199e7fb538eed156b02
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-12-13 21:52:25 -0500

    routes updated with crud operations

--- controllers/books.js
+++ controllers/books.js
@@ -2,36 +2,63 @@ const express = require ('express')
 const books = express.Router ()
 const Book = require ('../models/books.js')
 
+
 //INDEX
-books.get ('/', async (req, res) => {
-    res.send('get list of books')
+books.get ('/', (req, res) => {
+    Book.find ()
+    .then (books => {
+        res.status(200).json(books)
+    })
+    .catch (err => {
+        res.json(err)
+    })
+    
 })
 
 //Book Details
 books.get ('/:id', async (req, res) => {
-    res.send ('Show book details')
+    Book.findById (req.params.id)
+        .then (book => {
+            res.status(200).json(book)
+        })
+        .catch (err => {
+            res.json (err)
+        })
 })
 
 //Create
-books.post ('/', async (req, res) => { 
-    res.send ('Create new book')
+books.post ('/', (req, res) => { 
+    Book.create(req.body)
+        .then ( bookCreated => {
+            res.status (200).json (bookCreated)
+
+        })
+        .catch (err => {
+            res.send ('error is 404')
+        })
 })
 
 //Delete
 books.delete ('/:id', async (req, res) => {
-    res.send ('book deleted')
+    Book.findByIdAndDelete (req.params.id)
+        .then (book => {
+            res.status (200).json ('Delete Successful')
+        })
+        .catch (err => {
+            res.json (err)
+        })
 })
 
 //Update book details
 books.put ('/:id', async (req, res) => {
-    res.send ('book details upated')
+    Book.findByIdAndUpdate (req.params.id, req.body, {new: true})
+        .then (book => {
+            res.status (200).json (book)
+        })
+        .catch (err => {
+            res.json (err)
+        })
 })
-
-
-
-
-
-
 
 module.exports = books
 
